a81cc94d176ac2ae6f2b7aa5689734258f05e286
Anton <mordbergak@gmail.com>
2016-02-17 14:56:45 +0300

    Merge branch '01-string-set' of https://github.com/java-course-au/assignments-2016-1 into 01-string-set Add new tests Implementation completed #swag

--- src/main/java/ru/spbau/mit/StringSetImpl.java
+++ src/main/java/ru/spbau/mit/StringSetImpl.java
@@ -1,57 +1,106 @@
 package ru.spbau.mit;
 
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * @author antonpp
  * @since 16/02/16
  */
 public class StringSetImpl implements StringSet {
 
-    private static final int ALPHABET_SIZE = 23;
-    private final Set<String> set = new HashSet<>();
+    public static final int ALPHABET_SIZE = 2 * ('z' - 'a' + 1);
     private final Node root = new Node();
+
+    public static int charToIndex(char c) {
+        if (Character.isLowerCase(c)) {
+            return c - 'a';
+        } else {
+            return 'z' - 'a' + c - 'A' + 1;
+        }
+    }
 
     @Override
     public boolean add(String element) {
-//        return addX(element, 0, root);
-        return set.add(element);
+        return addX(element, root, 0);
     }
 
-    private boolean addX(String s, int i, Node node) {
-        return false;
+    private boolean addX(final String s, final Node node, int i) {
+        node.incCounter();
+        if (i == s.length()) {
+            final boolean res = !node.isLeaf();
+            node.setLeaf(true);
+            return res;
+        }
+        return addX(s, node.getNode(s.charAt(i)), i + 1);
     }
 
     @Override
     public boolean contains(String element) {
-        return set.contains(element);
+        return containsX(element, root, 0);
+    }
+
+    private boolean containsX(final String s, final Node node, int i) {
+        if (i == s.length()) {
+            return node.isLeaf();
+        }
+        final char c = s.charAt(i);
+        return node.hasNode(c) && containsX(s, node.getNode(c), i + 1);
     }
 
     @Override
     public boolean remove(String element) {
-        return set.remove(element);
+        if (contains(element)) {
+            removeX(element, root, 0);
+            return true;
+        }
+        return false;
+    }
+
+    private void removeX(final String s, final Node node, int i) {
+        node.decCounter();
+        if (i != s.length()) {
+            removeX(s, node.getNode(s.charAt(i)), i + 1);
+        }
     }
 
     @Override
     public int size() {
-        return set.size();
+        return root.getCounter();
     }
 
     @Override
     public int howManyStartsWithPrefix(String prefix) {
-        int n = 0;
-        for (String s : set) {
-            if (s.startsWith(prefix)) {
-                ++n;
-            }
+        return prefixX(prefix, root, 0);
+    }
+
+    public int prefixX(final String s, final Node node, int i) {
+        if (i == s.length()) {
+            return node.getCounter();
         }
-        return n;
+        final char c = s.charAt(i);
+        if (node.hasNode(c)) {
+            return prefixX(s, node.getNode(c), i + 1);
+        }
+        return 0;
     }
 
     private static final class Node {
-        private final char[] children = new char[ALPHABET_SIZE * 2];
+        private final Node[] children = new Node[ALPHABET_SIZE];
+        private int counter;
         private boolean isLeaf;
+
+        public int getCounter() {
+            return counter;
+        }
+
+        public void incCounter() {
+            ++counter;
+        }
+
+        public void decCounter() {
+            --counter;
+            if (counter == 0) {
+                isLeaf = false;
+            }
+        }
 
         public boolean isLeaf() {
             return isLeaf;
@@ -59,6 +108,18 @@ public class StringSetImpl implements StringSet {
 
         public void setLeaf(boolean isLeaf) {
             this.isLeaf = isLeaf;
+        }
+
+        public Node getNode(char c) {
+            final int index = charToIndex(c);
+            if (children[index] == null) {
+                children[index] = new Node();
+            }
+            return children[index];
+        }
+
+        public boolean hasNode(char c) {
+            return children[charToIndex(c)] != null;
         }
     }
 }
--- src/test/java/ru/spbau/mit/StringSetTest.java
+++ src/test/java/ru/spbau/mit/StringSetTest.java
@@ -1,24 +1,10 @@
 package ru.spbau.mit;
 
-import static org.junit.Assert.*;
 import org.junit.Test;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import static org.junit.Assert.*;
 
 public class StringSetTest {
-
-    @Test
-    public void testSimple() {
-        StringSet stringSet = instance();
-
-        assertTrue(stringSet.add("abc"));
-        assertTrue(stringSet.contains("abc"));
-        assertEquals(1, stringSet.size());
-        assertEquals(1, stringSet.howManyStartsWithPrefix("abc"));
-    }
 
     public static StringSet instance() {
         try {
@@ -31,5 +17,137 @@ public class StringSetTest {
             e.printStackTrace();
         }
         throw new IllegalStateException("Error while class loading");
+    }
+
+    @Test
+    public void testSimple() {
+        StringSet stringSet = instance();
+
+        assertTrue(stringSet.add("abc"));
+        assertTrue(stringSet.contains("abc"));
+        assertEquals(1, stringSet.size());
+        assertEquals(1, stringSet.howManyStartsWithPrefix("abc"));
+    }
+
+    @Test
+    public void testDuplicates() {
+        final StringSet stringSet = instance();
+        assertTrue(stringSet.add("abc"));
+        assertFalse(stringSet.add("abc"));
+        assertTrue(stringSet.add("abd"));
+        assertFalse(stringSet.add("abd"));
+
+        assertEquals(4, stringSet.howManyStartsWithPrefix("ab"));
+
+        assertTrue(stringSet.remove("abc"));
+        assertEquals(3, stringSet.howManyStartsWithPrefix("ab"));
+
+        assertFalse(stringSet.remove("ab"));
+        assertEquals(3, stringSet.howManyStartsWithPrefix("ab"));
+    }
+
+    @Test
+    public void testPrefix() {
+        final StringSet stringSet = instance();
+        assertEquals(0, stringSet.howManyStartsWithPrefix("ab"));
+        assertTrue(stringSet.add("abc"));
+        assertTrue(stringSet.add("abcdefghjiklmnoprst"));
+        assertEquals(1, stringSet.howManyStartsWithPrefix("abcde"));
+        assertEquals(2, stringSet.howManyStartsWithPrefix("a"));
+    }
+
+    @Test
+    public void testUpperCase() {
+        StringSet stringSet = instance();
+        assertTrue(stringSet.add("ABC"));
+        assertTrue(stringSet.contains("ABC"));
+        assertEquals(1, stringSet.size());
+        assertEquals(1, stringSet.howManyStartsWithPrefix("ABC"));
+        assertFalse(stringSet.add("ABC"));
+        assertEquals(0, stringSet.howManyStartsWithPrefix("BC"));
+        assertEquals(2, stringSet.howManyStartsWithPrefix("A"));
+    }
+
+    @Test
+    public void testCharToIndex() {
+        int index = 0;
+        for (char c = 'a'; c <= 'z'; ++c) {
+            assertTrue(index++ == StringSetImpl.charToIndex(c));
+        }
+        for (char c = 'A'; c <= 'Z'; ++c) {
+            assertTrue(index++ == StringSetImpl.charToIndex(c));
+        }
+        assertTrue(index == StringSetImpl.ALPHABET_SIZE);
+    }
+
+    @Test
+    public void testEmpty() {
+        final StringSet stringSet = instance();
+        assertTrue(stringSet.add("abc"));
+        assertTrue(stringSet.add("abcdefghjiklmnoprst"));
+        assertTrue(stringSet.add("sdgsadfasdf"));
+        assertTrue(stringSet.add("KJNDfsdfjnskdfADF"));
+        assertTrue(stringSet.add("asdvjnsKAJBDSSDF"));
+        assertTrue(stringSet.add("ABCDEFGHIJKLMNOPRST"));
+        assertTrue(stringSet.add("AKSHJDBVSD"));
+        assertTrue(stringSet.add("AAAAAAAAAAAAA"));
+        assertTrue(stringSet.add("ZZZZZZZZZ"));
+        assertTrue(stringSet.add("zzzzzzzzz"));
+        assertTrue(stringSet.add("aaaaaaaa"));
+        assertTrue(stringSet.add("KSJDfskdfjsdf"));
+        assertTrue(stringSet.add("popo"));
+
+        assertTrue(stringSet.remove("abc"));
+        assertTrue(stringSet.remove("abcdefghjiklmnoprst"));
+        assertTrue(stringSet.remove("sdgsadfasdf"));
+        assertTrue(stringSet.remove("KJNDfsdfjnskdfADF"));
+        assertTrue(stringSet.remove("asdvjnsKAJBDSSDF"));
+        assertTrue(stringSet.remove("ABCDEFGHIJKLMNOPRST"));
+        assertTrue(stringSet.remove("AKSHJDBVSD"));
+        assertTrue(stringSet.remove("AAAAAAAAAAAAA"));
+        assertTrue(stringSet.remove("ZZZZZZZZZ"));
+        assertTrue(stringSet.remove("zzzzzzzzz"));
+        assertTrue(stringSet.remove("aaaaaaaa"));
+        assertTrue(stringSet.remove("KSJDfskdfjsdf"));
+        assertTrue(stringSet.remove("popo"));
+
+        assertEquals(0, stringSet.size());
+
+        assertTrue(stringSet.add("abc"));
+        assertTrue(stringSet.add("abcdefghjiklmnoprst"));
+        assertTrue(stringSet.add("sdgsadfasdf"));
+        assertTrue(stringSet.add("KJNDfsdfjnskdfADF"));
+        assertTrue(stringSet.add("asdvjnsKAJBDSSDF"));
+        assertTrue(stringSet.add("ABCDEFGHIJKLMNOPRST"));
+        assertTrue(stringSet.add("AKSHJDBVSD"));
+        assertTrue(stringSet.add("AAAAAAAAAAAAA"));
+        assertTrue(stringSet.add("ZZZZZZZZZ"));
+        assertTrue(stringSet.add("zzzzzzzzz"));
+        assertTrue(stringSet.add("aaaaaaaa"));
+        assertTrue(stringSet.add("KSJDfskdfjsdf"));
+        assertTrue(stringSet.add("popo"));
+
+        assertEquals(13, stringSet.size());
+    }
+
+    @Test
+    public void testSize() {
+        final StringSet stringSet = instance();
+        assertEquals(0, stringSet.howManyStartsWithPrefix("ab"));
+        assertTrue(stringSet.add("abc"));
+        assertTrue(stringSet.add("abcdefghjiklmnoprst"));
+        assertTrue(stringSet.add("sdgsadfasdf"));
+        assertTrue(stringSet.add("KJNDfsdfjnskdfADF"));
+        assertTrue(stringSet.add("asdvjnsKAJBDSSDF"));
+        assertTrue(stringSet.add("ABCDEFGHIJKLMNOPRST"));
+        assertTrue(stringSet.add("AKSHJDBVSD"));
+        assertTrue(stringSet.add("AAAAAAAAAAAAA"));
+        assertTrue(stringSet.add("ZZZZZZZZZ"));
+        assertTrue(stringSet.add("zzzzzzzzz"));
+        assertTrue(stringSet.add("aaaaaaaa"));
+        assertTrue(stringSet.add("KSJDfskdfjsdf"));
+        assertTrue(stringSet.add("popo"));
+        assertEquals(stringSet.size(), stringSet.howManyStartsWithPrefix(""));
+        assertEquals(stringSet.size(), 13);
     }
 }
