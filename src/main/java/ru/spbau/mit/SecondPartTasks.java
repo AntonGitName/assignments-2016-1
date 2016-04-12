9f7f4973a0f2ef4300bc257c449e393576fe2467
Anton <mordbergak@gmail.com>
2016-04-12 04:22:24 +0300

    4 - streams

--- src/main/java/ru/spbau/mit/SecondPartTasks.java
+++ src/main/java/ru/spbau/mit/SecondPartTasks.java
@@ -1,15 +1,32 @@
 package ru.spbau.mit;
 
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public final class SecondPartTasks {
 
-    private SecondPartTasks() {}
+    private static final long SAMPLES_NUM = 1000_000L;
+    private static final double R = 0.5;
+
+    private SecondPartTasks() {
+    }
 
     // Найти строки из переданных файлов, в которых встречается указанная подстрока.
     public static List<String> findQuotes(List<String> paths, CharSequence sequence) {
-        throw new UnsupportedOperationException();
+        return paths.stream().flatMap(p -> {
+            try {
+                return Files.lines(Paths.get(p));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }).
+                filter(p -> p.contains(sequence)).
+                collect(Collectors.toList());
     }
 
     // В квадрат с длиной стороны 1 вписана мишень.
@@ -17,18 +34,26 @@ public final class SecondPartTasks {
     // Надо промоделировать этот процесс с помощью класса java.util.Random и посчитать,
     // какова вероятность попасть в мишень.
     public static double piDividedBy4() {
-        throw new UnsupportedOperationException();
+        final Function<Double, Double> sqr = x -> x * x;
+        final PrimitiveIterator.OfDouble it = new Random().doubles().iterator();
+        return Stream.generate(() -> sqr.apply(it.next() - R) + sqr.apply(it.next() - R)).limit(SAMPLES_NUM).
+                filter(distSqr -> distSqr < R * R).count() / (double) SAMPLES_NUM;
     }
 
     // Дано отображение из имени автора в список с содержанием его произведений.
     // Надо вычислить, чья общая длина произведений наибольшая.
     public static String findPrinter(Map<String, List<String>> compositions) {
-        throw new UnsupportedOperationException();
+        return compositions.entrySet().stream().
+                collect(Collectors.toMap(Map.Entry::getKey,
+                        e -> e.getValue().stream().mapToInt(String::length).sum())).
+                entrySet().stream().max(Comparator.comparing(Map.Entry::getValue)).map(Map.Entry::getKey).
+                orElse("");
     }
 
     // Вы крупный поставщик продуктов. Каждая торговая сеть делает вам заказ в виде Map<Товар, Количество>.
     // Необходимо вычислить, какой товар и в каком количестве надо поставить.
     public static Map<String, Integer> calculateGlobalOrder(List<Map<String, Integer>> orders) {
-        throw new UnsupportedOperationException();
+        return orders.stream().map(Map::entrySet).flatMap(Set::stream).
+                collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingInt(Map.Entry::getValue)));
     }
 }
--- src/test/java/ru/spbau/mit/SecondPartTasksTest.java
+++ src/test/java/ru/spbau/mit/SecondPartTasksTest.java
@@ -1,28 +1,76 @@
 package ru.spbau.mit;
 
+import com.google.common.collect.ImmutableMap;
+import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.fail;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 
 public class SecondPartTasksTest {
 
+    private static final double EPS = 1e-3;
+
     @Test
     public void testFindQuotes() {
-        fail();
+        final List<String> paths = Arrays.asList(
+                "src/test/resources/a.txt",
+                "src/test/resources/b.txt",
+                "src/test/resources/c.txt"
+        );
+        final List<String> expected = Arrays.asList(
+                "import java.util.*;",
+                "import java.util.function.Function;",
+                "import java.util.stream.Collectors;",
+                "import java.util.stream.IntStream;",
+                "import java.util.stream.Stream;",
+                "import com.google.common.collect.ImmutableMap;",
+                "import org.junit.Test;",
+                "import java.util.Arrays;",
+                "import java.util.Collections;",
+                "import java.util.Optional;",
+                "import java.util.stream.Collectors;",
+                "import java.util.stream.IntStream;",
+                "import java.util.stream.Stream;",
+                "import static junitx.framework.Assert.assertEquals;",
+                "import static ru.spbau.mit.FirstPartTasks.*;"
+        );
+        Assert.assertEquals(expected, SecondPartTasks.findQuotes(paths, "import"));
     }
 
     @Test
     public void testPiDividedBy4() {
-        fail();
+        Assert.assertEquals(Math.PI / 4, SecondPartTasks.piDividedBy4(), EPS);
     }
 
     @Test
     public void testFindPrinter() {
-        fail();
+        final Map<String, List<String>> test = ImmutableMap.of(
+                "1", Arrays.asList("1", "22", "333"),
+                "2", Arrays.asList("22", "4444"),
+                "3", Arrays.asList("55555")
+        );
+        Assert.assertEquals("1", SecondPartTasks.findPrinter(test));
     }
 
     @Test
     public void testCalculateGlobalOrder() {
-        fail();
+        final List<Map<String, Integer>> test = Arrays.asList(ImmutableMap.of(
+                        "1", 1,
+                        "2", 2,
+                        "3", 3
+                ), ImmutableMap.of(
+                        "1", 4,
+                        "2", 5,
+                        "3", 6
+                )
+        );
+        final ImmutableMap<String, Integer> expected = ImmutableMap.of(
+                "1", 5,
+                "2", 7,
+                "3", 9
+        );
+        Assert.assertEquals(expected, SecondPartTasks.calculateGlobalOrder(test));
     }
 }
