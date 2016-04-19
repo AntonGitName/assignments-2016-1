fd0e71f57bdf97832c9a254f6d8d1e99af89c18c
Anton <mordbergak@gmail.com>
2016-04-19 14:52:32 +0300

    test-02-injector

--- src/main/java/ru/spbau/mit/Injector.java
+++ src/main/java/ru/spbau/mit/Injector.java
@@ -1,6 +1,8 @@
 package ru.spbau.mit;
 
-import java.util.List;
+import java.lang.reflect.Constructor;
+import java.util.*;
+import java.util.stream.Collectors;
 
 
 public final class Injector {
@@ -12,6 +14,33 @@ public final class Injector {
      * `implementationClassNames` for concrete dependencies.
      */
     public static Object initialize(String rootClassName, List<String> implementationClassNames) throws Exception {
-        throw new IllegalStateException();
+        final Class rootCls = Class.forName(rootClassName);
+        final Constructor constructor = rootCls.getDeclaredConstructors()[0];
+        final Class[] dependencies = constructor.getParameterTypes();
+
+        final List<Object> implObjects = new ArrayList<>(implementationClassNames.size());
+        for (String className : implementationClassNames) {
+            implObjects.add(Class.forName(className).newInstance());
+        }
+
+        final Map<Class, Object> dependencySolver = new HashMap<>(dependencies.length);
+        for (Class dependency : dependencies) {
+            Object resolver = null;
+            for (Object impl: implObjects) {
+                if (dependency.isInstance(impl)) {
+                    if (resolver != null) {
+                        throw new AmbiguousImplementationException();
+                    } else {
+                        resolver = impl;
+                    }
+                }
+            }
+            if (resolver == null) {
+                throw new ImplementationNotFoundException();
+            }
+            dependencySolver.put(dependency, resolver);
+        }
+        final List<Object> cstrArgs = Arrays.stream(dependencies).map(dependencySolver::get).collect(Collectors.toList());
+        return constructor.newInstance(cstrArgs.toArray());
     }
 }
